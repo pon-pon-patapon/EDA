03b5c0d5f3796d364ca6882d7749da6b8890d315
pon-pon-patapon <61442541+pon-pon-patapon@users.noreply.github.com>
2024-11-12 15:00:14 +0900

    app.py

--- app.py
+++ app.py
@@ -22,17 +22,17 @@ if uploaded_file:
     # Perturbationが1のデータを黒い線でプロット
     perturbation_1 = data[data["Perturbation"] == 1]
     fig.add_trace(go.Scatter(x=perturbation_1["Time_sec"], y=perturbation_1["EDA"],
-                             mode="lines", line=dict(color="black", width=2),
+                             mode="lines", line=dict(color="white", width=0.5),
                              name="Perturbation: 1"), row=1, col=1)
 
     # Perturbationが2のデータを青い線でプロット
     perturbation_2 = data[data["Perturbation"] == 2]
     fig.add_trace(go.Scatter(x=perturbation_2["Time_sec"], y=perturbation_2["EDA"],
-                             mode="lines", line=dict(color="blue", width=2),
+                             mode="lines", line=dict(color="blue", width=0.5),
                              name="Perturbation: 2"), row=1, col=1)
 
     # Dig1が1のポイントを赤い丸で強調表示
-    dig1_points = data[data["Dig1"] == 1]
+    dig1_points = data[data["Dig1"] == 5]
     fig.add_trace(go.Scatter(x=dig1_points["Time_sec"], y=dig1_points["EDA"],
                              mode="markers", marker=dict(color="red", size=10, line=dict(color="black", width=1)),
                              name="Dig1 Points"), row=1, col=1)
